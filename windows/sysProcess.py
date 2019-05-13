7be3cbd00f9f783124bae6c2d8b3f3f1d17f7730
ytwei <249422@qq.com>
2019-05-13 15:06:58 +0800

    co-debug the result with java service

--- windows/sysProcess.py
+++ windows/sysProcess.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.7
 # coding=utf-8
 import os
-
+import sys
 
 def getValue(t):
     try:
@@ -19,81 +19,81 @@ def getSCValue(line):
 
 def process(result):
     output = os.system("systeminfo >system_info.log")
-    line = open("system_info.log", "r").readlines()
+    lineArray = open("system_info.log", "r").readlines()
 
-    result["OS Host Name"] = getValue(line[1])
-    result["OS Name"] = getValue(line[2])
-    result["OS Version"] = getValue(line[3])
-    result["OS Manufacturer"] = getValue(line[4])
-    result["OS Configuration"] = getValue(line[5])
-    result["OS Build Type"] = getValue(line[6])
-    result["OS Registered Owner"] = getValue(line[7])
-    result["OS Registered Organization"] = getValue(line[8])
-    result["OS Product ID"] = getValue(line[9])
-    result["OS Original Install Date"] = getValue(line[10])
-    result["System Boot Time"] = getValue(line[11])
-    result["System Manufacturer"] = getValue(line[12])
-    result["System Model"] = getValue(line[13])
-    result["System Type"] = getValue(line[14])
+    result["OS Host Name"] = getValue(lineArray[1])
+    result["OS Name"] = getValue(lineArray[2])
+    result["OS Version"] = getValue(lineArray[3])
+    result["OS Manufacturer"] = getValue(lineArray[4])
+    result["OS Configuration"] = getValue(lineArray[5])
+    result["OS Build Type"] = getValue(lineArray[6])
+    result["OS Registered Owner"] = getValue(lineArray[7])
+    result["OS Registered Organization"] = getValue(lineArray[8])
+    result["OS Product ID"] = getValue(lineArray[9])
+    result["OS Original Install Date"] = getValue(lineArray[10])
+    result["System Boot Time"] = getValue(lineArray[11])
+    result["System Manufacturer"] = getValue(lineArray[12])
+    result["System Model"] = getValue(lineArray[13])
+    result["System Type"] = getValue(lineArray[14])
 
     lineNum = 16
-    for i in range(lineNum, len(line)):
-        if line[i][0] != ' ':
+    for i in range(lineNum, len(lineArray)):
+        if lineArray[i][0] != ' ':
             lineNum = i
             break
-    result["System Processor(s)"] = [i.strip() for i in line[16:lineNum]]
+    result["System Processor(s)"] = [i.strip() for i in lineArray[16:lineNum]]
 
-    result["System BIOS Version"] = getValue(line[lineNum])
+    result["System BIOS Version"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Windows Directory"] = getValue(line[lineNum])
+    result["OS Windows Directory"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["System Directory"] = getValue(line[lineNum])
+    result["System Directory"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["System Boot Device"] = getValue(line[lineNum])
+    result["System Boot Device"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Locale"] = getValue(line[lineNum])
+    result["OS Locale"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Input Locale"] = getValue(line[lineNum])
+    result["OS Input Locale"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Time Zone"] = getValue(line[lineNum])
+    result["OS Time Zone"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["System Total Physical Memory"] = getValue(line[lineNum])
+    result["System Total Physical Memory"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["System Available Physical Memory"] = getValue(line[lineNum])
+    result["System Available Physical Memory"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Virtual Memory: Max Size"] = getValue(line[lineNum])
+    result["OS Virtual Memory: Max Size"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Virtual Memory: Available"] = getValue(line[lineNum])
+    result["OS Virtual Memory: Available"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Virtual Memory: In Use"] = getValue(line[lineNum])
+    result["OS Virtual Memory: In Use"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Page File Location(s)"] = getValue(line[lineNum])
+    result["OS Page File Location(s)"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Domain"] = getValue(line[lineNum])
+    result["OS Domain"] = getValue(lineArray[lineNum])
     lineNum = lineNum + 1
 
-    result["OS Logon Server"] = getValue(line[lineNum])
+    result["OS Logon Server"] = getValue(lineArray[lineNum])
 
     lineNum = lineNum + 2
 
-    for i in range(lineNum, len(line)):
-        if line[i][0] != ' ':
+    for i in range(lineNum, len(lineArray)):
+        if lineArray[i][0] != ' ':
             endNum = i
             break
-    result["OS Hotfix(s)"] = [i.strip() for i in line[lineNum:endNum]]
+    result["OS Hotfix(s)"] = [i.strip() for i in lineArray[lineNum:endNum]]
 
     output = os.system("sc query wscsvc >system_Alerter.log")
     result['System Alerter'] = getSCValue(open("system_Alerter.log", 'r').readlines())
